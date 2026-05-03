bb922ddef568542ee24a09aa308d37df27095161
Awang <me@karisma.id>
2026-05-03 22:47:30 +0000

    ci: auto detect base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [tailwindcss(), solid(), Icons({ compiler: 'solid' })],
 })
